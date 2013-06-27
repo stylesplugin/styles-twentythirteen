5ab5df66c46f48afff6be295e123173c730b50bc
Paul Clark <pdclark@brainstormmedia.com>
2013-06-27 02:08:37 -0800

    Suppress notices for unset  variable

--- styles-child-notices.php
+++ styles-child-notices.php
@@ -138,7 +138,7 @@ class Styles_Child_Notices {
 	 */
 	public function install_notice() {
 		if ( 'update.php' == basename( $_SERVER['PHP_SELF'] )
-			|| 'delete-selected' == $_GET['action']
+			|| 'delete-selected' == @$_GET['action']
 			|| !current_user_can('install_plugins')
 		) {
 			return false;
@@ -168,7 +168,7 @@ class Styles_Child_Notices {
 	 */
 	public function activate_notice() {
 		if ( 'update.php' == basename( $_SERVER['PHP_SELF'] )
-			|| 'delete-selected' == $_GET['action']
+			|| 'delete-selected' == @$_GET['action']
 			|| !current_user_can('install_plugins')
 		) {
 			return false;
@@ -193,7 +193,7 @@ class Styles_Child_Notices {
 	 */
 	public function upgrade_notice() {
 		if ( 'update.php' == basename( $_SERVER['PHP_SELF'] )
-			|| 'delete-selected' == $_GET['action']
+			|| 'delete-selected' == @$_GET['action']
 			|| !current_user_can('install_plugins')
 		) {
 			return false;
